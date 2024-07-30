e1f7ec7613fb1e37537bb823312b1e33eb406f31
Matheus Alcance <limarios18@outlook.com>
2024-07-30 16:37:58 -0300

    Criando grafico mensal

--- Dashboard.py
+++ Dashboard.py
@@ -3,6 +3,8 @@ import requests
 import pandas as pd
 import plotly.express as px
 
+st.set_page_config(layout= 'wide')
+
 # Função para formatar valores:
 def formata_numero(valor, prefixo = ''):
     for unidade in ['', 'mil']:
@@ -20,9 +22,16 @@ url = 'https://labdados.com/produtos'
 response = requests.get(url)
 dados = pd.DataFrame.from_dict(response.json())
 
+# Formatando data para datetime:
+dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format='%d/%m/%Y')
+
 ## Tabelas
 receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
 receita_estados = dados.drop_duplicates(subset= 'Local da compra')[['Local da compra', 'lat', 'lon']].merge(receita_estados, left_on='Local da compra', right_index= True).sort_values('Preço', ascending=False)
+
+receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq= 'M'))['Preço'].sum().reset_index()
+receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
+receita_mensal['Mes'] = receita_mensal['Data da Compra'].dt.month_name()
 
 ## Gráficos
 fig_mapa_receita = px.scatter_geo(receita_estados,
@@ -35,15 +44,26 @@ fig_mapa_receita = px.scatter_geo(receita_estados,
                                   hover_data= {'lat': False, 'lon': False},
                                   title = 'Receita por estado')
 
+fig_receita_mensal = px.line(receita_mensal,
+                             x = 'Mes',
+                             y = 'Preço',
+                             markers= True,
+                             range_y = (0, receita_mensal.max()),
+                             color= 'Ano',
+                             line_dash= 'Ano',
+                             title= 'Receita mensal')
+
+fig_receita_mensal.update_layout(yaxis_title = 'Receita')
 
 ## Visualização no streamlit
 # Aplicando filtros e metricas:
 coluna1, coluna2 = st.columns(2)
 with coluna1:
     st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
-    st.plotly_chart(fig_mapa_receita)
+    st.plotly_chart(fig_mapa_receita, use_container_width= True)
 with coluna2:
     st.metric('Quantidade de vendas', formata_numero(dados.shape[0]))
+    st.plotly_chart(fig_receita_mensal, use_container_width= True)
 
 # Exibindo dataframe na aplicação
 st.dataframe(dados)
